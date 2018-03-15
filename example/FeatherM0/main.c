dd8f769ad6fbb30b46d9c8f054b9fd202669e73c
Ethan Slattery <theslat@gmail.com>
2018-03-14 18:03:38 -0700

    updated USb library and changed output format to be more CSV like

--- example/FeatherM0/main.c
+++ example/FeatherM0/main.c
@@ -1,6 +1,5 @@
 #include <atmel_start.h>
 #include <stdio.h>
-#include "usb_start.h"
 #include "si705x.h"
 
 #define MESSAGE_SIZE		(64)
@@ -12,27 +11,32 @@ int main(void)
 	float celc;
 	int whole, decimal;
 	uint16_t rawTemp;
+    int32_t err;
 	
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 	si705x_init(&wire);
 	
-	while (1) {
-		/* Turn on LED if the DTR signal is set (serial terminal open on host) */
-		gpio_set_pin_level(LED_BUILTIN, usb_dtr());
+	for(;;) {
+        if(usb_state() == USB_Configured) {
+		    /* Turn on LED if the DTR signal is set (serial terminal open on host) */
+		    gpio_set_pin_level(LED_BUILTIN, usb_dtr());
 		
-		rawTemp = si705x_measure();
-		celc    = si705x_celsius(rawTemp);
-		whole   = (int)celc;
-		decimal = (celc * TEMP_PRECISION) - (whole * TEMP_PRECISION);
+		    rawTemp = si705x_measure();
+		    celc    = si705x_celsius(rawTemp);
+		    whole   = (int)celc;
+		    decimal = (celc * TEMP_PRECISION) - (whole * TEMP_PRECISION);
 					
-		sprintf(message, "SI7051: 0x%.4X (%d.%d %cC)\n", rawTemp, whole, decimal, 0xB0);
+		    sprintf(message, "%d.%d,0x%.4X\n",whole, decimal, rawTemp);
 		
-		/* If the host has a terminal open then try to read, and if we read send it back */
-		if(usb_dtr()) {								
-			usb_send_buffer((uint8_t*)message, strlen(message));
-		}
+		    /* If the host has a terminal open then try to read, and if we read send it back */
+		    if(usb_dtr()) {
+                do {					
+			    err = usb_write(message, strlen(message));
+                } while(err < 0);
+		    }
 
-		delay_ms(1000);
+		    delay_ms(250);
+        }
 	}
 }
